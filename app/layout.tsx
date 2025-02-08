8d616566b44468844353c91261123e9616d5406d
Dharani Gowtham <dharanigowthamsampath@gmail.com>
2025-02-08 19:35:53 +0530

    updated layout vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,18 +38,22 @@ export default function RootLayout({
           content="A free and open resource for developers."
         />
         <Script
+          strategy="afterInteractive"
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-51LPZXWYLT"
-        ></Script>
-        <Script id="google-analytics">
-          {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-51LPZXWYLT');
-          `}
-        </Script>
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-51LPZXWYLT');
+            `,
+          }}
+        />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
